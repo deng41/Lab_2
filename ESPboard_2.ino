5f0e112e671230c3ba4c834b62487c2353d395a1
deng41 <459629361@qq.com>
2016-10-20 20:53:58 -0700

    Update ESPboard_2.ino

--- ESPboard_2.ino
+++ ESPboard_2.ino
@@ -2,67 +2,47 @@
 #define echoPin 16
 #define infredPin 5
 int preLevel, preInfred;
-void setup() {
+void setup() {              //initialize
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(infredPin, INPUT);
   preLevel = 0;
   preInfred = 1;
-//  pinMode(led, OUTPUT);
-//  pinMode(led2, OUTPUT);
 }
 
 void loop() {
   int infredoff, level;
   long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, LOW); //set trigger signal for distance sensor  
+  delayMicroseconds(2); 
   digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(1000); - Removed this line
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2)/29.1;
-//  if (distance < 4) {  // This is where the LED On/Off happens
-//    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
-//  digitalWrite(led2,LOW);
-//}
-//  else {
-//    digitalWrite(led,LOW);
-//    digitalWrite(led2,HIGH);
-//  }
-//  if (distance >= 200 || distance <= 0){
-//    Serial.println("Out of range");
-//  }
-//  else {
-//    Serial.print(distance);
-//    Serial.println(" cm");
-//  }
 
-//Serial.println(distance);
-//Serial.println("dis");
-infredoff=digitalRead(infredPin);
-level = distance / 10;
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW); //check echo signal to get the duration in microsecond
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2)/29.1; //convert duration to distance in cm
+
+
+
+infredoff=digitalRead(infredPin); //check infrared signal
+level = distance / 10; //map the distance to different levels
 if(level > 7)
 {
   level = 7;
 }
-//Serial.println("level");
-//Serial.println(level);
-//Serial.println("infredoff");
-//Serial.println(infredoff);
-level=level+48;
+
+level=level+48; //convert integer to ASCII
 infredoff=infredoff+48;
 
+//Send signal to UART only if the sensor signals change
 if((level != preLevel) || (infredoff != preInfred))
 {
-  Serial.write('S');
+  Serial.write('S'); //preamble for the signal
   Serial.write('D');
   Serial.write(level);
   Serial.write('A');
   Serial.write(infredoff);
-  //Serial.println(infredoff);
 }
 preLevel = level;
 preInfred = infredoff;
